d0e18d6f158e025198e695754e9784f5f090aa85
227131 <227131@KB37-120-C12>
2025-01-21 15:24:29 +0300

    Работа

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -39,7 +40,49 @@ namespace _522_Yatsuk_Katan
         //Реализация функции расчета
         private void calc_btn_Click(object sender, RoutedEventArgs e)
         {
+            // Превращает текст из полей в числа. Значение сток сокраняется в переменные x и y
+            if (!double.TryParse(input_x.Text, out double x) || !double.TryParse(input_y.Text, out double y))
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения для x и m.", "Ошибка ввода");
+                return;
+            }
+            // Сохраняет результат выполения функции в переменную result
+            double result = CalculateFunction(x, y);
+            // Преобразует число в строку
+            result_tb.Text = result.ToString();
+        }
+        // Короч, это сам прмер. Вооооооть
+        private double CalculateFunction(double x, double y)
+        {
+            Func<double, double> f = GetSelectedFunction();
 
+            if (x - y == 0)
+            {
+                return Math.Pow(f(x), 2) + Math.Pow(y, 2) + Math.Sin(y);
+            }
+            else if (x - y > 0)
+            {
+                return Math.Pow(f(x) - y, 2) + Math.Cos(y);
+            }
+            else if (x - y < 0)
+            {
+                return Math.Pow(y - f(x), 2) + Math.Tan(y);
+            }
+            else
+            {
+                return 0; // Или как обработать некорректные условия
+            }
+        }
+        private Func<double, double> GetSelectedFunction()
+        {
+            if (func_1.IsChecked == true)
+                return x => Math.Sinh(x);
+            else if (func_2.IsChecked == true)
+                return x => Math.Pow(x, 2);
+            else if (func_3.IsChecked == true)
+                return x => Math.Exp(x);
+            else
+                return x => 0; // По умолчанию
         }
 
         //---
